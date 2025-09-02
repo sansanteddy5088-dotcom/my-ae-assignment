95dc1eb37922f781f5e343cd5e5fa6611687e1ae
sherrina <sherrina.putri@binus.ac.id>
2025-09-02 17:27:46 +0700

    assignment

---
+++ fyp2.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main(){
+	
+	char name[100];
+	char program[100];
+	
+	printf("My name is Sherrina Hardianto Putri\n");
+	printf("My favorite programming language is JavaScript\n");
+	
+	return 0;
+}
